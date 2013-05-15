244fbcb3664d133916a32516f5c9e87b33d427a9
Ryan Kelker <theinternet@yandex.com>
2013-05-15 10:40:00 +0900

    changed telex packet creation

--- README.md
+++ README.md
@@ -1,13 +1,32 @@
 # th1
 
-A Clojure library designed to ... well, that part is up to you.
+Work in progress
 
-## Usage
+```clojure
+th1.telex> 
+(def telex-data
+  (tdata
+   {:commands (hash-map "CommandExample" "abc")
+    :signals (hash-map "SignalExample" "def")
+    :headers (hash-map "HeaderExample" "ghi")}))
+#'th1.telex/telex-data
+th1.telex> 
+(clojure.pprint/pprint
+ (packet {:-tdata telex-data}))
+{:_to nil,
+ :_br nil,
+ :_ring nil,
+ :_line nil,
+ :_hop nil,
+ :+SignalExample "def",
+ :.CommandExample "abc",
+ :_HeaderExample "ghi"}
+nil
+th1.telex> 
+(clojure.pprint/pprint
+ (packet {:remove-nil? true 
+	  :-tdata telex-data}))
+{:+SignalExample "def", :.CommandExample "abc", :_HeaderExample "ghi"}
+nil
 
-FIXME
-
-## License
-
-Copyright © 2013 FIXME
-
-Distributed under the Eclipse Public License, the same as Clojure.
+```
--- src/th1/telex.clj
+++ src/th1/telex.clj
@@ -15,27 +15,28 @@
                   ^PersistentHashMap commands
                   ^PersistentHashMap headers])
 (defn tdata
-   [signals-hmap
-    commands-hmap
-    headers-hmap]
-   (let [signals (when signals-hmap
+  "Data to add to Telex. All args are hmaps."
+   [{:keys [signals
+            commands
+            headers]}]
+   (let [signals (when signals
                    (zipmap
                     (map #(str "+" (name %))
-                         (keys signals-hmap))
-                    (vals signals-hmap)))
-         commands (when commands-hmap
+                         (keys signals))
+                    (vals signals)))
+         commands (when commands
                     (zipmap
                      (map #(str "." (name %))
-                          (keys commands-hmap))
-                     (vals commands-hmap)))
-         headers (when headers-hmap
+                          (keys commands))
+                     (vals commands)))
+         headers (when headers
                    (zipmap
                     (map #(str "_" (name %))
-                         (keys headers-hmap))
-                    (vals headers-hmap)))]
+                         (keys headers))
+                    (vals headers)))]
     (TData. signals commands headers)))
 
-(defrecord Packet [_to _br _ring _line _hop tdata])
+(defrecord Packet [_to _br _ring _line _hop])
 
 (extend-type Packet
   Telex
@@ -48,8 +49,31 @@
   (line [_] (:_line _))
   (hop [_] (:_hop _)))
 
-(defn packet [{:keys [to br ring line hop -tdata]}]
-  (Packet. to br ring line hop (or -tdata
-                                   (tdata nil nil nil))))
+(defn packet [{:keys [to br ring line hop -tdata remove-nil?]}]
+  (let [{:keys
+         [signals 
+          commands 
+          headers] :as att} -tdata
+          attributes (apply merge (vals att))
+          telex (loop [p (Packet. to br ring line hop)
+                       a attributes]
+                  (if-not (seq a) p
+                          (let [_ (first a)
+                                [k v] [(key _) (val _)]]
+                            (recur
+                             (assoc p (keyword k) v)
+                             (rest a)))))]
+    (if-not remove-nil? telex
+            (loop [t telex]
+              (if-not (->> t
+                           vals
+                           (some nil?))
+                t
+                (let [_ (first t)
+                      [k v] [(key _) (val _)]]
+                  (recur
+                   (if (nil? v)
+                     (dissoc t k)))))))))
+           
 
 
